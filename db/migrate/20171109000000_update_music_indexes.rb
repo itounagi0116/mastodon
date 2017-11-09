929180b03776ad0b3cc9720044e5f7d43f87c980
ABCanG <abcang1015@gmail.com>
2017-11-09 18:24:49 +0900

    Fix migration of music status

--- db/migrate/20171109000000_update_music_indexes.rb
+++ db/migrate/20171109000000_update_music_indexes.rb
@@ -1,9 +1,11 @@
 class UpdateMusicIndexes < ActiveRecord::Migration[5.1]
+  disable_ddl_transaction!
+
   def change
-    add_index :statuses, [:account_id, :music_type, :id]
-    remove_index :statuses, [:music_type, :account_id]
-    remove_index :statuses, [:music_type, :id]
-    remove_index :statuses, [:reblog_of_id, :music_type, :music_id]
+    add_index :statuses, [:account_id, :music_type, :id], algorithm: :concurrently
+    remove_index :statuses, column: [:music_type, :account_id], algorithm: :concurrently
+    remove_index :statuses, column: [:music_type, :id], algorithm: :concurrently
+    remove_index :statuses, column: [:reblog_of_id, :music_type, :music_id], algorithm: :concurrently
     remove_index :album_tracks, :album_id
   end
 end
