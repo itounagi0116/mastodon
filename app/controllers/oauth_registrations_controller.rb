4e5518ddcc664c0d02650549d05f3cf703b664e5
ABCanG <abcang1015@gmail.com>
2017-11-20 19:47:05 +0900

    Remove DefaultFollowWorker

--- app/controllers/oauth_registrations_controller.rb
+++ app/controllers/oauth_registrations_controller.rb
@@ -15,7 +15,6 @@ class OauthRegistrationsController < DeviseController
     if @oauth_registration.save
       BootstrapTimelineWorker.perform_async(@oauth_registration.user.account_id)
       sign_in(@oauth_registration.user)
-      DefaultFollowWorker.perform_async(@oauth_registration.user.account_id)
 
       redirect_to after_sign_in_path_for(@oauth_registration.user)
     elsif @oauth_registration.errors.added?(:email, :taken)
