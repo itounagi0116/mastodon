b6b622761b684072dd6130f4f5326cf6480411ba
ABCanG <abcang1015@gmail.com>
2017-11-27 18:12:02 +0900

    Fix WebPush toggle css

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -44,7 +44,7 @@ module.exports = {
 
   plugins: [
     new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
-    new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-statichash-f59224258fd59e28eb604471523ea66d.css' : '[name].css'),
+    new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-statichash-d93b1469b57271601414e1e1556ccc43.css' : '[name].css'),
     new ManifestPlugin({
       publicPath: output.publicPath,
       writeToFileEmit: true,
