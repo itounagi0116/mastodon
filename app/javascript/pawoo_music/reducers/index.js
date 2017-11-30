c5bb64259583b9cb59a3d6ebed186221bf37e755
abcang <abcang1015@gmail.com>
2017-11-30 22:28:22 +0900

    Update index.js

--- app/javascript/pawoo_music/reducers/index.js
+++ app/javascript/pawoo_music/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux-immutable';
 import acct_map from './acct_map';
 import album_compose from './album_compose';
 import track_compose from './track_compose';
-import tracks from './tracks';
 import account_gallery from './account_gallery';
 import player from './player';
 import account_tracks from './account_tracks';
@@ -14,7 +13,6 @@ export default combineReducers({
   acct_map,
   album_compose,
   track_compose,
-  tracks,
   account_gallery,
   player,
   account_tracks,
