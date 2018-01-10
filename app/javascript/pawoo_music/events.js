adfe487211ed8cadfb5968912678ae1a4d81e148
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-01-10 10:28:33 +0900

    Add event tags to genre_tag_picker

---
+++ app/javascript/pawoo_music/events.js
@@ -0,0 +1,11 @@
+import Immutable from 'immutable';
+
+export default Immutable.fromJS([
+  {
+    start_date: new Date('2017/12/29'),
+    end_date: new Date('2017/12/31'),
+    hashtag: 'C93',
+  },
+]);
+
+// TODO: サーバで設定可能にする
