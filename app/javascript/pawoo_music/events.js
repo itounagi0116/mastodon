7195f8fad8b707e7284c253ace8400cdb16232b8
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-05-14 15:43:09 +0900

    Add APOLLO08 to events

--- app/javascript/pawoo_music/events.js
+++ app/javascript/pawoo_music/events.js
@@ -6,6 +6,11 @@ export default Immutable.fromJS([
     end_date: new Date('2018/04/29'),
     hashtag: '2018M3春',
   },
+  {
+    start_date: new Date('2018/06/01'),
+    end_date: new Date('2018/06/03'),
+    hashtag: 'APOLLO08',
+  },
 ]);
 
 // TODO: サーバで設定可能にする
