3f65de157730886b75963bc4bea2508c6ddaae40
abcang <abcang1015@gmail.com>
2018-09-03 18:21:16 +0900

    Delete playlist

--- app/lib/exceptions.rb
+++ app/lib/exceptions.rb
@@ -6,15 +6,6 @@ module Mastodon
   class ValidationError < Error; end
   class RaceConditionError < Error; end
   class MusicConvertError < Error; end
-  class MusicSourceNotFoundError < Error; end
-  class MusicSourceForbiddenError < Error; end
-  class MusicSourceFetchFailedError < Error; end
-  class PlayerControlLimitError < Error; end
-  class PlayerControlSkipLimitTimeError < Error; end
-  class PlaylistWriteProtectionError < Error; end
-  class PlaylistSizeOverError < Error; end
-  class PlaylistItemNotFoundError < Error; end
-  class RedisMaxRetryError < Error; end
   class TrackNotFoundError < Error; end
 
   class MusicvideoError < MusicConvertError; end
