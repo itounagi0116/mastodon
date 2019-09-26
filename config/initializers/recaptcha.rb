1ebb43aa3a866fc4582de98327edc8073ff0fbf3
abcang <abcang1015@gmail.com>
2019-09-26 13:40:11 +0900

    remove comment

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -4,5 +4,5 @@ Recaptcha.configure do |config|
   config.site_key = ENV['RECAPTCHA_SITE_KEY']
   config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
   # 開発環境で試す場合はrecaptchaの設定画面から`localhost`をドメインに追加して、skip_verify_envをコメントアウトする
-  # config.skip_verify_env << 'development'
+  config.skip_verify_env << 'development'
 end
